f99117cee11da432ec70879642560c318c71ef29
itggot-markus-moen <markus.moen@itggot.se>
2019-02-07 10:29:36 +0100

    Added a register and login system.

--- app.rb
+++ app.rb
@@ -8,3 +8,56 @@ enable :sessions
 get('/') do
     slim(:index)
 end
+
+post('/login') do
+    db = SQLite3::Database.new('db/blogg.db')
+    db.results_as_hash = true
+
+    username = params["Username"]
+    password = params["Password"]
+   
+    username_db = db.execute("SELECT Username from Users WHERE Username = (?)", username)
+    password_db = db.execute("SELECT Password from Users WHERE Username = (?)", username)
+    if username_db != nil && password_db != nil
+        username_db = username_db[0][0]
+        password_db = password_db[0][0]
+    else
+        redirect("/denied")
+    end    
+    if username_db == username
+        if BCrypt::Password.new(password_db) == password
+            session["logged_in"] = true
+            redirect("/granted")
+        end
+    end
+    redirect("/denied")
+end
+
+get('/granted') do
+    if session["logged_in"] == true
+        slim(:granted)
+    else
+        redirect('/denied')
+    end
+end
+
+get('/denied') do
+    slim(:denied)
+end
+
+get('/register') do
+    slim(:register)
+end
+
+post('/newuser') do
+    db = SQLite3::Database.new('db/blogg.db')
+    db.results_as_hash = true
+
+    secret_password = BCrypt::Password.create(params["Password"])
+    username = params["Username"]
+    email = params["Email"]
+
+    db.execute("INSERT INTO Users(Username, Password, Email) VALUES(?, ?, ?)", username, secret_password, email)
+
+    redirect('/')
+end
